6faad8d7113b9e10e5ef5d16bd0980d4c80dedf5
nischalhp <nischal.hp@gmail.com>
2016-11-05 21:06:52 +0530

    Adding mentions and user counts

--- geeksrus/analytics/wordcloud.py
+++ geeksrus/analytics/wordcloud.py
@@ -1,7 +1,6 @@
 import pandas as pd
 from geeksrus import LOGGER
-from geeksrus.utils.dbconn import read_mongo
-from geeksrus.utils.dbconn import write_mongo
+from geeksrus.utils.dbconn import read_mongo, write_mongo, read_mongo_projection
 from geeksrus import dbcon
 from geeksrus import cfg
 
@@ -12,7 +11,10 @@ from datetime import datetime
 import traceback
 
 stop = stopwords.words('english')
-stop.extend(('.', ',', '"', "'", '?', '!', ':', ';', '(', ')', '[', ']', '{', '}','/','-', '@', '#','://', 'https', 'RT', 'co', '&', '...'))
+stop.extend(('.', ',', '"', "'", '?', '!', ':', ';',
+             '(', ')', '[', ']', '{', '}','/','-', '@', '#','://',
+             'https', 'RT', 'co', '&', '...'
+             , 'say', 'says'))
 
 
 class WordCloud:
@@ -24,6 +26,7 @@ class WordCloud:
             for data in data_df['text']:
                 tokens = nltk.wordpunct_tokenize(data)
                 for token in tokens:
+                    token = token.strip()
                     if token.lower() not in stop:
                         if token in frequency_words_wo_stop:
                             count = frequency_words_wo_stop[token]
@@ -51,7 +54,70 @@ class WordCloud:
         except Exception as e:
             LOGGER.error(traceback.format_exc())
 
+    def getMentionsFrequency(self):
+        try:
+            projection = []
+            projection.append('entities.user_mentions')
+            data = read_mongo_projection(dbcon, cfg['collection'], query=projection)
+            data.to_csv('trials.csv')
+            normalized_df = pd.io.json.json_normalize(data.entities, 'user_mentions')
+            grouped_df = normalized_df.groupby(by=['screen_name'], as_index=False)['id'].count()
+            grouped_df_screen_count = grouped_df[['screen_name','id']]
+            grouped_df_screen_count = grouped_df_screen_count.sort(columns='id', ascending=False).reset_index()
+            grouped_df_screen_count = grouped_df_screen_count.ix[:20,]
+            count_data = []
+
+            for row in grouped_df_screen_count.iterrows():
+                screen_count = {}
+                screen_count["name"] = row[1]['screen_name']
+                screen_count["count"] = int(row[1]['id'])
+                count_data.append(screen_count)
+
+            mongo_doc = {}
+            mongo_doc['word_cloud'] = count_data
+            mongo_doc['timestamp'] = datetime.now()
+            print(mongo_doc)
+
+            write_mongo(db_conn=dbcon, collection=cfg['mentions_collection'], document=mongo_doc)
+
+        except Exception as e:
+            LOGGER.error(traceback.format_exc())
+
+    def getMaximumUserCount(self):
+        try:
+            projection = []
+            projection.append('user.screen_name')
+            projection.append('user.id')
+            data = read_mongo_projection(dbcon, cfg['collection'], query=projection)
+            column_df = data.apply(self.generate_columns_from_dict, axis=1)
+            grouped_df = column_df.groupby(by=['screen_name'], as_index=False)['id'].count()
+            grouped_df_screen_count = grouped_df[['screen_name','id']]
+            grouped_df_screen_count = grouped_df_screen_count.sort(columns='id', ascending=False).reset_index()
+            grouped_df_screen_count = grouped_df_screen_count.ix[:20,]
+            count_data = []
+
+            for row in grouped_df_screen_count.iterrows():
+                screen_count = {}
+                screen_count["name"] = row[1]['screen_name']
+                screen_count["count"] = int(row[1]['id'])
+                count_data.append(screen_count)
+
+            mongo_doc = {}
+            mongo_doc['word_cloud'] = count_data
+            mongo_doc['timestamp'] = datetime.now()
+
+            write_mongo(db_conn=dbcon, collection=cfg['users_collection'], document=mongo_doc)
+
+        except Exception as e:
+            LOGGER.error(traceback.format_exc())
+
+    def generate_columns_from_dict(self, row):
+        user_dict = dict(row['user'])
+        return pd.Series({'screen_name': user_dict['screen_name'], 'id': user_dict['id']})
+
 
 if __name__ == "__main__":
     wc = WordCloud()
     wc.getTokenFrequency()
+    wc.getMentionsFrequency()
+    wc.getMaximumUserCount()
